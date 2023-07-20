e664185234da10ec758d9561e0a0be8075156bda
Kal <kal.aalrajhi@gmail.com>
2023-07-20 15:05:32 -0400

    DRY up newJoke fetch

--- NetworkTestI/Jokes API/FetchData.swift
+++ NetworkTestI/Jokes API/FetchData.swift
@@ -26,43 +26,53 @@ class JokesViewModel: ObservableObject {
         ///       a. check that there is actually data at endpoint
         ///       b. check that there is a valid HTTP URL Response code. If there is, then check that it's a 2xx status code.
         ///       c. check for any error messages
+        downloadData(fromURL: url) { returnedData in
+            if let data = returnedData {
+                
+        /// 3) Decode JSON data into newJoke data model so it can be used in our app
+                guard let newJoke = try? JSONDecoder().decode(Joke.self, from: data) else {
+                    print("Unable to decode data into Joke model, please check that properties in data model match API property values.")
+                    return
+                }
+                print("Decoded JSON: \(newJoke)")
+        
+        /// 4) Ensure our data appears all at once in our UI
+                DispatchQueue.main.async { [weak self] in
+                    self?.randomJokes.append(newJoke)
+                }
+            }
+        }
+    }
+    
+    func downloadData(fromURL url: URL, completionHandler: @escaping (_ data: Data?) -> ()) {
+        
         URLSession.shared.dataTask(with: url) { (data, response, error) in
-            
             guard let data = data else {
                 print("No data available.")
+                completionHandler(nil)
                 return
             }
             
             guard let response = response as? HTTPURLResponse else {
                 print("Uknown response type. Expected response should be of type HTTPURLResponse.")
+                completionHandler(nil)
                 return
             }
             
             guard response.statusCode >= 200 && response.statusCode < 300 else {
                 print("Response code \(response.statusCode) returned.")
+                completionHandler(nil)
                 return
             }
             
             guard error == nil else {
                 print("Error found: \(String(describing: error))")
+                completionHandler(nil)
                 return
             }
             
-            /// 3) Decode JSON data into data model so it can be used in our app
-            guard let newJoke = try? JSONDecoder().decode(Joke.self, from: data) else {
-                print("Unable to decode data into Joke model, please check that properties in data model match API property values.")
-                return
-            }
-            
-            print("Decoded JSON: \(newJoke)")
-            
-            /// 4) Ensure our data appears all at once in our UI
-            ///     Note: Concurrency - Ensure the code below is executed on main thread in a non-blocking way (by default URLSession task goes on the background thread, which we want to override here)
-            ///         This allows our array to store data and update it in the UI async. If we just ran self.randomJokes.append(newJoke) on its own, it would update random parts at random times.
-            ///         Similar to Promise.all([]), which queues up all the responses and stores them at once.
-            DispatchQueue.main.async { [weak self] in
-                self?.randomJokes.append(newJoke)
-            }
+            // Success - return data
+            completionHandler(data)
         }.resume() // resume starts the URLSession data task
     }
 }
